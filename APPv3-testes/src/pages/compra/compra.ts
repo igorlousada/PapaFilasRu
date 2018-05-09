04c8d8146a2f22ddb612fd4c46c3e3f6f56616d7
julia_jamile@hotmail.com <Jucagu10>
2018-05-09 11:20:20 -0300

    APP para testes

---
+++ APPv3-testes/src/pages/compra/compra.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+
+@Component({
+  selector: 'page-compra',
+  templateUrl: 'compra.html'
+})
+export class CompraPage {
+
+  constructor(public nav: NavController) {
+  }
+
+}
