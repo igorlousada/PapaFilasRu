a3792709eb2e001d22e69eb6efaa226e54532dd1
Júlia Jamile <julia_jamile@hotmail.com>
2018-05-10 14:48:12 -0300

    App.v3
    ainda não esta pronto

---
+++ APP/projetos/src/pages/cardapio/cardapio.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+
+@IonicPage()
+@Component({
+  selector: 'page-cardapio',
+  templateUrl: 'cardapio.html',
+})
+export class CardapioPage {
+
+  constructor(public nav: NavController) {
+  }
